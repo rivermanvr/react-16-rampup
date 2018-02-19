56a1024159bc7f095654a4a9a798c50939dfa501
rivermanvr <vrgithub@riversconsulting.com>
2018-02-19 12:51:00 -0500

    simplifyng app.js

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
           <h1 className="App-title">React</h1>
-        </header>
       </div>
     );
   }
