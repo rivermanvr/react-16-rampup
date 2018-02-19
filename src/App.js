57a4547c26e94f2fa039c2b8723b82a1559a4c4e
rivermanvr <vrgithub@riversconsulting.com>
2018-02-19 12:52:55 -0500

    simplifying App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <h1 className="App-title">React</h1>
+          <h1>React App</h1>
       </div>
     );
   }
