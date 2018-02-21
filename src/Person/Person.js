c2e0cfb0ef4dbafd5a1ad39f678bbae8ca71abd1
rivermanvr <vrgithub@riversconsulting.com>
2018-02-21 10:18:11 -0500

    added a props.children to the person component

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const person = ({ name, age }) => {
-  return <p>My name is { name } and I am { age } years old!</p>
+  return (
+    <div>
+      <p>My name is { name } and I am { age } years old!</p>
+      <p>{ props.children }</p>
+    </div>
+  )
 }
 
 export default person;
