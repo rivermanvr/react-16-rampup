ab5faa52f02630689c0974fa9ddfd29a7b02271a
rivermanvr <vrgithub@riversconsulting.com>
2018-02-21 15:28:42 -0500

    calling component with state variable

--- src/App.js
+++ src/App.js
@@ -17,8 +17,8 @@ class App extends Component {
         <h1>React App</h1>
         <button>Switch Name</button>
         <Person name = { person[0].name } age = { person[0].age } />
-        <Person name = "Karen" age = "35" />
-        <Person name = "Alex" age = "32">Where does this go?</Person>
+        <Person name = { person[1].name } age = { person[1].age } />
+        <Person name = { person[2].name } age = { person[2].age }>Where does this go?</Person>
       </div>
     );
   }
