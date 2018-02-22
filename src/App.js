6a8de2e253b7e40814f883ee51119abaa7d77bbc
rivermanvr <vrgithub@riversconsulting.com>
2018-02-22 10:06:29 -0500

    added an onClick listener and passed it to the component

--- src/App.js
+++ src/App.js
@@ -10,15 +10,28 @@ class App extends Component {
       { name: 'Karen', age: 40 }
     ]
   }
+  switchNameHandler = () => {
+    console.log('I clicked the button')
+  }
+
   render() {
     const person = this.state.persons;
     return (
       <div className="App">
-        <h1>React App</h1>
-        <button>Switch Name</button>
-        <Person name = { person[0].name } age = { person[0].age } />
-        <Person name = { person[1].name } age = { person[1].age } />
-        <Person name = { person[2].name } age = { person[2].age }>Where does this go?</Person>
+        <h1>React 16 Experiments</h1>
+        <button onClick={ this.switchNameHandler }>Switch Name</button>
+        <Person
+          name = { person[0].name }
+          age = { person[0].age } />
+        <Person
+          name = { person[1].name }
+          age = { person[1].age } />
+        <Person
+          name = { person[2].name }
+          age = { person[2].age }
+          click = { this.switchNameHandler }>
+          This string will pass in props.children?
+        </Person>
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-const person = ({ name, age, children }) => {
+const person = ({ name, age, click, children }) => {
   return (
     <div>
-      <p>My name is { name } and I am { age } years old!</p>
+      <p onClick={ click }>
+        My name is { name } and I am { age } years old!
+      </p>
       <p>{ children }</p>
     </div>
   )
