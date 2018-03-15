a9b5e749c8b7eba4bd070f9844f3234c75997404
rivermanvr <vrgithub@riversconsulting.com>
2018-03-15 11:56:09 -0400

    added () for multi-line expression

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -4,13 +4,13 @@ import Person from './Person/Person'
 class Persons extends Component {
   render() {
     return this.props.persons.map((person, index) => {
-      return <Person
+      return (<Person
         click={ () => this.props.clicked(index) }
         changed={ (ev) => this.props.changed(ev, person.id) }
         name={ person.name }
         age={ person.age }
         key={ index }
-      />;
+      />);
     });
   }
 }
