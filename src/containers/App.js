422f2a8b8e77280560baaceba905c89af4a673e1
rivermanvr <vrgithub@riversconsulting.com>
2018-03-16 10:50:39 -0400

    showing order of life cycle hooks...

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,15 @@ import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log('[App.js] - inside constructor', props);
+  }
+
+  componentWillMount() {
+    console.log('[App.js] - inside componentWillMount');
+  }
+
   state = {
     persons: [
       { id: 332, name: 'Max', age: 28 },
