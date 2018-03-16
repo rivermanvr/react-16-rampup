7ca6d7b7218bfe32a39b5bee2f330b14586744b5
rivermanvr <vrgithub@riversconsulting.com>
2018-03-16 11:08:08 -0400

    ilustrating inside Person component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -2,7 +2,21 @@ import React, { Component } from 'react';
 import styles from './Person.css';
 
 class Person extends Component {
+  constructor(props) {
+    super(props);
+    console.log('[Person.js] - inside constructor', props);
+  }
+
+  componentWillMount() {
+    console.log('[Person.js] - inside componentWillMount()');
+  }
+
+  componentDidMount() {
+    console.log('[Person.js] - inside componentDidMount()');
+  }
+
   render() {
+    console.log('[Person.js] - inside render()');
     return (
       <div className={ styles.Person }>
         <p onClick={ this.props.click }>
